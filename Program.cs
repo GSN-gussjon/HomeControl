ab7c8ba61efe1e325e60ab466530ad4d40af39e8
Gussjon <50756250+Gussjon@users.noreply.github.com>
2022-11-27 18:14:57 +0100

    Cm2

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -13,20 +14,24 @@ namespace HomeControl
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            var httpClient = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
+
+            httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Access-Control-Allow-Origin", "*");
+
+            builder.Services.AddScoped(sp => httpClient);
+
 
             builder.Services.AddCors(o =>
             {
                 o.AddDefaultPolicy(policy =>
                                    {
-                                       policy.WithOrigins("https://gsnha.duckdns.org:8123").AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+                                       policy.WithOrigins("https://gsnha.duckdns.org:8123").AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().Build();
                                    });
+                
             });
 
 
             var app = builder.Build();
-
-
             await app.RunAsync();
         }
     }
