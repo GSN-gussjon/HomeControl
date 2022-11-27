bb21673c81ec31ed566f110998420e083d57cc80
Gussjon <50756250+Gussjon@users.noreply.github.com>
2022-11-27 20:26:43 +0100

    Test cors

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using HADotNet.Core;
+using HADotNet.Core.Clients;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -15,24 +17,11 @@ namespace HomeControl
 
             var httpClient = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
 
-            var a1 = httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Access-Control-Allow-Origin", "*");
-            var a2 = httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Access-Control-Allow-Headers", "*");
-            var a3 = httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Access-Control-Expose-Headers", "*");
-            var a4 = httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Access-Control-Allow-Methods", "*");
-            var a5 = httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Access-Control-Allow-Credentials", "true");
-
             builder.Services.AddScoped(sp => httpClient);
 
+            builder.Services.AddSingleton(sp => ClientFactory.GetClient<StatesClient>());
 
-            builder.Services.AddCors(o =>
-            {
-                o.AddDefaultPolicy(policy =>
-                                   {
-                                       policy.WithOrigins("https://gsnha.duckdns.org:8123").AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().Build();
-                                   });
-                
-            });
-
+            ClientFactory.Initialize("https://gsnha.duckdns.org:8123", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI5NzZmZWRmNjA5ZGM0YTk5YjY3MDkzZDY2OGQzMTk1MSIsImlhdCI6MTY2OTU2MDYxOCwiZXhwIjoxOTg0OTIwNjE4fQ.d7gA8Xglhv6SCCAfkylB9eM_qykuT_bNpze8wWVZhGY");
 
             var app = builder.Build();
             await app.RunAsync();
